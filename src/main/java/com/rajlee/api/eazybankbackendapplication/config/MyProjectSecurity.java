7ec5e473e44168243b1fd2a621825a1e3eefe60b
priyanshu <imraj383foru@gmail.com>
2024-04-30 19:30:22 +0530

    second commit using inmemory and multiple users

--- src/main/java/com/rajlee/api/eazybankbackendapplication/config/MyProjectSecurity.java
+++ src/main/java/com/rajlee/api/eazybankbackendapplication/config/MyProjectSecurity.java
@@ -4,11 +4,17 @@ package com.rajlee.api.eazybankbackendapplication.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class MyProjectSecurity {
 
+    // This method is just for custom configuration , with only one user form application properties
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests()
@@ -17,5 +23,34 @@ public class MyProjectSecurity {
                 .and().formLogin()
                 .and().httpBasic();
                 return http.build();
+    }
+
+    // Approach 1 :This method is implemented for multiple users is there , this is not for production environment
+//    @Bean
+//    InMemoryUserDetailsManager userDetailsService(){
+//        UserDetails admin = User.withDefaultPasswordEncoder()
+//                .username("priyanshu")
+//                .password("prishu")
+//                .authorities("admin")
+//                .build();
+//        UserDetails user = User.withDefaultPasswordEncoder()
+//                .username("sonali")
+//                .password("12345")
+//                .authorities("read")
+//                .build();
+//        return new InMemoryUserDetailsManager(admin,user);
+//    }
+
+    //Approach2 : similarly for above bean there is another method to write
+    @Bean
+    InMemoryUserDetailsManager userDetailsService(){
+        UserDetails admin = User.withUsername("priyanshu").password("prishu").authorities("admin").build();
+        UserDetails user = User.withUsername("sonali").password("12345").authorities("read").build();
+        return new InMemoryUserDetailsManager(admin,user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
     }
 }
